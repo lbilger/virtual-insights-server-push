55b0e61d4e2ee4085bb60768c5ad358845f38e6f
Lars Bilger <lars.bilger@lhind.dlh.de>
2020-12-09 22:37:12 +0100

    initial state from Spring Initializr

---
+++ src/main/kotlin/com/lhind/virtualinsights/serverpushmongo/ServerpushMongoApplication.kt
@@ -0,0 +1,11 @@
+package com.lhind.virtualinsights.serverpushmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerpushMongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerpushMongoApplication>(*args)
+}
---
+++ src/test/kotlin/com/lhind/virtualinsights/serverpushmongo/ServerpushMongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lhind.virtualinsights.serverpushmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerpushMongoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
